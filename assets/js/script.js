4a2b81bba3ef26b16779043c6a566d09eda71f00
Michael <mbahl1670@gmail.com>
2021-10-31 21:24:56 -0500

    created an array for questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var quizQA = [
+  {
+    question: "What is the name of the fictional town Buffy the Vampire Slayer takes place in?",
+    answer1: "Mystic Falls",
+    answer2: "Sunnydale",
+    answer3: "Riverdale",
+    answer4: "Paradise",
+    correctAnswer: "Sunnydale"
+  },
+  {
+    question: "What is the name of Buffy's best friend?",
+    answer1: "Betty Cooper",
+    answer2: "Elena Gilbert",
+    answer3: "Cordelia Chase",
+    answer4: "Willow Rosenthal",
+    correctAnswer: "Willow Rosenthal"
+  },
+  {
+    question: "What is the name of the actress who played Buffy?",
+    answer1: "Sara Michelle Geller",
+    answer2: "Eliza Dushku",
+    answer3: "Alyson Hannigan",
+    answer4: "Nina Dobrev",
+    correctAnswer: "Sara Michelle Geller"
+  },
+  {
+    question: "Who sired Angel, the first vampire Buffy fell in love with?",
+    answer1: "Stephen",
+    answer2: "The Master",
+    answer3: "Darla",
+    answer4: "Dracula",
+    correctAnswer: "Darla"
+  },
+  {
+    question: "What type of monster never appeard on Buffy",
+    answer1: "Witch",
+    answer2: "Godzilla",
+    answer3: "Werewolf",
+    answer4: "Demon",
+    correctAnswer: "Godzilla"
+  },
+  {
+    question: "According to Xander in the episode 'Prophecy Girl', what kind of music is the music of pain?",
+    answer1: "Country",
+    answer2: "Death Metal",
+    answer3: "Korean Pop",
+    answer4: "Classical",
+    correctAnswer: "Country"
+  }
+];
